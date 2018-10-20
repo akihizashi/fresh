0803d95c376879b2ac09c6509e7c787ef0c18869
gict <gict@cbc.local>
2018-10-20 17:28:29 +0900

    navbar

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.master')
 @include('layouts.navbar')
 @section('content')
-    <img src="/storage/images/banner.jpg" alt="" class="img-fluid">
+    <img src="/storage/images/banner2.jpg" alt="" class="img-fluid">
 @endsection
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,13 +1,10 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <a class="navbar-brand" href="#">Fresh fruit</a>
-    <div class="hamburger">
+    <div class="hamburger" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <div class="top-bun"></div>
         <div class="meat"></div>
         <div class="bottom-bun"></div>
     </div>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-    </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
