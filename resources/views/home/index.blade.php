a1c8f3e7acf7bf74507a4270033d1daf79123eca
gict <gict@cbc.local>
2018-10-20 15:50:44 +0900

    first

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.master')
+@include('layouts.navbar')
+@section('content')
+    <img src="/storage/images/banner.jpg" alt="" class="img-fluid">
+@endsection
---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <a class="navbar-brand" href="#">Fresh fruit</a>
+    <div class="hamburger">
+        <div class="top-bun"></div>
+        <div class="meat"></div>
+        <div class="bottom-bun"></div>
+    </div>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Shop</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+            </li>
+        </ul>
+    </div>
+</nav>
