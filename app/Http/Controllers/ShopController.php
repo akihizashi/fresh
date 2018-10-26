6044b3daf3e3f014cc530bd79d6b44353ed867fe
akihizashi <longluu.ftu@gmail.com>
2018-10-27 08:52:43 +0900

    shop database

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Shop;
 
 class ShopController extends Controller
 {
     public function index()
     {
-        return view('/shop.index');
+        $products = Shop::paginate(24);
+        return view('/shop.index', compact('products'));
     }
 }
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,26 +1,30 @@
 <div class="container-fluid container-footer fixed-bottom">
   <div class="row pt-3">
-    <div class="col-sm-6 col-md-3 text-left">
-      <h6 class="text-light">Fresh fruit shop</h6>
-      <hr class="main-hr">
-      <p>Address: 000-0000 example andress</p>
-      <p>Tel: 000-000-0000</p>
-      <p>Email: example@email.com</p>
+    <div class="col-sm-6 col-md-9 text-left">
     </div>
-    <div class="col-sm-6 col-md-9 text-right">
-      <a class="text-light" href="#">
-          <i class="ml-2 mr-2 mt-0 icon-fb" data-feather="facebook"></i>
-      </a>
-      <a class="text-light" href="#">
-          <i class="ml-2 mr-2 mt-0 icon-tw" data-feather="twitter"></i>
-      </a>
-      <a class="text-light" href="#">
-          <i class="ml-2 mr-2 mt-0 icon-in" data-feather="instagram"></i>
-      </a>
+    <div class="col-sm-6 col-md-3 text-right">
+        <h6 class="text-light">Fresh fruit shop</h6>
+        <hr class="main-hr">
+        <p class="mb-0">
+            <i data-feather="home"></i> 123-0000 example andress
+            <i data-feather="phone"></i> 987-000-0000
+            <i data-feather="mail"></i> example@email.com
+        </p>
     </div>
   </div>
   <div class="text-center">
       <hr class="main-hr">
-      <p>Copyright 2018</p>
+      <p>
+          Copyright 2018
+          <a class="text-light" href="#">
+              <i class="ml-2 mr-2 mt-0 icon-fb" data-feather="facebook"></i>
+          </a>
+          <a class="text-light" href="#">
+              <i class="ml-2 mr-2 mt-0 icon-tw" data-feather="twitter"></i>
+          </a>
+          <a class="text-light" href="#">
+              <i class="ml-2 mr-2 mt-0 icon-in" data-feather="instagram"></i>
+          </a>
+      </p>
   </div>
 </div>
